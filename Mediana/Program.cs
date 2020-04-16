b36e9334f291ba222127f6199a1935b7912b7b4f
Bartosz Rosadzinski <voltermc@gmail.com>
2020-04-17 00:38:27 +0200

    Reworked color determination

--- Mediana/Program.cs
+++ Mediana/Program.cs
@@ -12,8 +12,7 @@ namespace Mediana
 {
     class Program
     {
-        // Replace 2560x1440 with your screen resolution, 
-        // but don't touch the 20x12 one
+        // Replace 2560x1440 with your screen resolution
         static Bitmap BigScreen = new Bitmap(2560, 1440);
         static Bitmap SmallScreen = new Bitmap(1, 1);
 
@@ -45,7 +44,7 @@ namespace Mediana
             RunUpdateThread = true;
             Stopwatch = new Stopwatch();
             ActualFpsMeter = new Stopwatch();
-            PotentialFpsMeter = new Stopwatch();
+            PotentialFpsMeter = new Stopwatch(); 
             UpdateThread.Start();
             Console.WriteLine("Running Mediana. Press any key or close this window to exit.\n\n");
             Console.ReadKey();
@@ -67,12 +66,19 @@ namespace Mediana
             }
 
             // Get the color of the squeezed pixel
-            System.Drawing.Color pixel = SmallScreen.GetPixel(0, 0);
+            System.Drawing.Color pixelRGB = SmallScreen.GetPixel(0, 0);
 
-            // Scale the color so that one of the base colors value is always 255
-            byte max = Math.Max(pixel.R, Math.Max(pixel.G, pixel.B));
-            double scale = 255.0 / max;
-            RGB.NET.Core.Color color = new RGB.NET.Core.Color((byte)(scale * pixel.R), (byte)(scale * pixel.G), (byte)(scale * pixel.B));
+            // Convert RGB -> HSV
+            HSV pixelHSV = new HSV (pixelRGB);
+
+            // Crank up saturation
+            //if (pixelHSV.H > 0.1)
+            //    pixelHSV.S = 1.0;
+
+            // Convert HSV -> RGB
+            System.Drawing.Color newPixelRGB = pixelHSV.ToRGB();
+            
+            RGB.NET.Core.Color color = new RGB.NET.Core.Color((newPixelRGB.R), (newPixelRGB.G), (newPixelRGB.B));
 
             // Assign the color to all the LEDs in your ledGroup
             ledGroup.Brush = new SolidColorBrush(color);         
@@ -103,6 +109,92 @@ namespace Mediana
                     Console.WriteLine("FPS limit: " + FPS_LIMIT + ", Actual FPS: " + ActualFps.ToString("n2") + ", Potential FPS: " + PotentialFps.ToString("n2"));
                 }
             }
+        }
+    }
+
+    class HSV
+    {
+        public double H, S, V;
+
+        public HSV (double H, double S, double V)
+        {
+            this.H = H;
+            this.S = S;
+            this.V = V;
+        }
+
+        public HSV (System.Drawing.Color rgb)
+        {
+            double r = rgb.R / 255.0;
+            double g = rgb.G / 255.0;
+            double b = rgb.B / 255.0;
+            double max = Math.Max(Math.Max(r, g), b);
+            double min = Math.Min(Math.Min(r, g), b);
+            double delta = max - min;
+
+            V = max;
+
+            if (delta < 0.000000001)
+            {
+                H = 0;
+                S = 0;
+                return;
+            }
+
+            if (max > 0.0)
+                S = delta / max;
+            else
+            {
+                H = 0;
+                S = 0;
+                return;
+            }
+
+            if (r == max)
+                H = (g - b) / delta;
+            else if (g == max)
+                H = 2.0 + (b - r) / delta;
+            else
+                H = 4.0 + (r - g) / delta;
+
+            H *= 60.0;
+            H += H < 0.0 ? 360.0 : 0.0;
+        }
+
+        public System.Drawing.Color ToRGB ()
+        {
+            if (S == 0.0)
+            {
+                return System.Drawing.Color.FromArgb((byte)(255*V), (byte)(255 * V), (byte)(255 * V));
+            }
+
+            double h = H;
+            h = h > 360.0 ? 0.0 : h;
+            h /= 60.0;
+            byte hInt = (byte)Math.Floor(h);
+            double hDec = h - hInt;
+            double x = V * (1.0 - S);
+            double y = V * (1.0 - (S * hDec));
+            double z = V * (1.0 - (S * (1.0 - hDec)));
+
+            switch (hInt)
+            {
+                case 0:
+                    return System.Drawing.Color.FromArgb((byte)(255.0 * V), (byte)(255.0 * z), (byte)(255.0 * x));
+                case 1:
+                    return System.Drawing.Color.FromArgb((byte)(255.0 * y), (byte)(255.0 * V), (byte)(255.0 * x));
+                case 2:
+                    return System.Drawing.Color.FromArgb((byte)(255.0 * x), (byte)(255.0 * V), (byte)(255.0 * z));
+                case 3:
+                    return System.Drawing.Color.FromArgb((byte)(255.0 * x), (byte)(255.0 * y), (byte)(255.0 * V));
+                case 4:
+                    return System.Drawing.Color.FromArgb((byte)(255.0 * z), (byte)(255.0 * x), (byte)(255.0 * V));
+                case 5:
+                    return System.Drawing.Color.FromArgb((byte)(255.0 * V), (byte)(255.0 * x), (byte)(255.0 * y));
+                default:
+                    return System.Drawing.Color.Black;
+            }
+
         }
     }
 }
